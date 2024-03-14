1851ef8b481d3f2a809c87a7df941cf856e34d15
erosalpe <81533597+erosalpe@users.noreply.github.com>
2024-03-14 12:31:40 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.basic')
+
+@section('title', 'Benvenuto | Home')
+
+@section('style')
+    <style lang="scss">
+       
+    </style>
+@endsection
+
+@section('main')
+    <h1>Ciao</h1>
+@endsection
+
+
